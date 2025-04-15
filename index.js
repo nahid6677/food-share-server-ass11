34dedcb2d9998b831712cba395f0f06155bb0dd2
nahid6677 <mdrajarr025@gmail.com>
2025-04-15 18:26:42 +0600

    genarate token

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ require("dotenv").config()
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
+
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -34,9 +39,27 @@ async function run() {
 
         const foodCollections = client.db("Food-Collection").collection("food");
 
+        app.post("/jwt", (req, res) => {
+            const userEmail = req.body;
+            // console.log(userEmail,"ddddddddd")
+            const token = jwt.sign(userEmail, process.env.JWT_SECRET, { expiresIn: "30m" })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+                .send({ message: true, logIn: true })
+        })
+        app.post("/logout", (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: false
+            })
+                .send({ message: true, logOut: true })
+        })
+
         app.post("/foods", async (req, res) => {
             const food = req.body;
-            const {expireDate, ...newFood} = food;
+            const { expireDate, ...newFood } = food;
             newFood.date = new Date(expireDate)
             const result = await foodCollections.insertOne(newFood);
             res.send(result);
@@ -48,18 +71,18 @@ async function run() {
             if (userEmail) {
                 query = { donerEmail: userEmail }
             }
-            const result =await foodCollections.find(query).toArray();
+            const result = await foodCollections.find(query).toArray();
             res.send(result);
         })
-        app.get("/foods/:id", async(req,res)=>{
+        app.get("/foods/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await foodCollections.findOne(query)
             res.send(result);
         })
-        app.delete("/food/:id", async(req,res)=>{
+        app.delete("/food/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await foodCollections.deleteOne(query);
             res.send(result);
         })
